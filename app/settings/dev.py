1897e8b2060fba7685997d7c4137d1df378e4ab8
sdk <dbsdk721@mail.ru>
2023-03-31 19:56:54 +0300

    start

---
+++ app/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-q2ag7jgcl(abc4o63j8qkjz*xz^5%!#7^bpp*4y^c)38akk@la"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
